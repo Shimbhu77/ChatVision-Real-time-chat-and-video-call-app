2ea4e7a007a3f8e4e90db25e7a20605aa70dc8f9
Shimbhu77 <kumawatshimbhu4@gmail.com>
2023-02-19 19:43:02 +0530

    Project started

---
+++ ChatVision/src/main/java/com/chatvision/ChatVisionApplication.java
@@ -0,0 +1,13 @@
+package com.chatvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatVisionApplication.class, args);
+	}
+
+}
---
+++ ChatVision/src/test/java/com/chatvision/ChatVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
